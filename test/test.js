cd1d5033df9f07e4a61e8ea0ca51d2d9cf68b16a
haoxin <coderhaoxin@outlook.com>
2014-04-30 17:30:00 +0800

    add support for symlinked directory

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 var path = require('path')
 
 var read = require('../')
@@ -25,6 +26,41 @@ describe('fs.readdirSyncRecursive()', function () {
       'package.json',
       'README.md'
     ].sort())
+
+    done()
+  })
+
+  it('should work in the symlinked directory', function (done) {
+    var linkname = __dirname + '-link'
+    fs.symlinkSync(__dirname, linkname, 'dir')
+
+    var files = read(linkname)
+
+    files.length.should.equal(1)
+    files[0].should.equal('test.js')
+
+    fs.unlinkSync(linkname)
+    done()
+  })
+
+  it('should work in the symlinked directory with a filter', function (done) {
+    var linkname = path.join(__dirname, '..') + '-link'
+    fs.symlinkSync(path.join(__dirname, '..'), linkname, 'dir')
+
+    var files = read(linkname, function (name) {
+      return name[0] !== '.' && name !== 'node_modules'
+    })
+
+    files.length.should.equal(5)
+    files.sort().should.eql([
+      'test/test.js',
+      'index.js',
+      'Makefile',
+      'package.json',
+      'README.md'
+    ].sort())
+
+    fs.unlinkSync(linkname)
 
     done()
   })
