ddf2662b6066f509231ddd905183b709a652186d
Yiyu He <dead_horse@qq.com>
2017-11-10 09:46:42 -0600

    feat: filter add dir as param (#8)

--- test/test.js
+++ test/test.js
@@ -15,12 +15,27 @@ describe('fs.readdirSyncRecursive()', function () {
 
   it('should work at the root with a filter', function () {
     var files = read(path.join(__dirname, '..'), function (name) {
-      return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage'
+      return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage' && name !== 'package-lock.json'
     })
 
     files.length.should.equal(5)
     files.sort().should.eql([
       'test/test.js',
+      'index.js',
+      'LICENSE',
+      'package.json',
+      'README.md'
+    ].sort())
+
+  })
+
+  it('should filter pass dir', function () {
+    var files = read(path.join(__dirname, '..'), function (name, index, dir) {
+      return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage' && dir !== __dirname && name !== 'package-lock.json'
+    })
+
+    files.length.should.equal(4)
+    files.sort().should.eql([
       'index.js',
       'LICENSE',
       'package.json',
@@ -69,7 +84,7 @@ describe('fs.readdirSyncRecursive()', function () {
       fs.symlinkSync(path.join(__dirname, '..'), linkname, 'dir')
 
       var files = read(linkname, function (name) {
-        return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage'
+        return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage' && name !== 'package-lock.json'
       })
 
       files.length.should.equal(5)
